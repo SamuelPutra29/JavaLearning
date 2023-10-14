4ed5a5cb5a0485f27b9e2652def38c42b2f79420
Samuel Putra <samueldianpermanaputra@gmail.com>
2023-10-14 11:29:48 +0200

    JavaTutorial

---
+++ src/Basic/HelloWorld.java
@@ -0,0 +1,8 @@
+// Ini adalah basic programming untuk membuat output
+package Basic;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
---
+++ src/Basic/variable/Variablle.java
@@ -0,0 +1,28 @@
+//Ini adalah class untuk menjelaskan variable di Java
+/*
+Jenis-jenis Tipe Data dalam Bahasa Java
+Secara garis besar, terdapat 2 kelompok tipe data dalam bahasa Java, yakni tipe data sederhana (Primitive data types), dan tipe data kompleks / object (Non-primitive data types).
+
+Primitive data type, terdiri dari tipe data berikut:
+
+Tipe data Integer: Tipe data untuk angka bulat seperti 5, 7, atau 48.
+Tipe data Float/Double: Tipe data untuk angka pecahan seperti 3.14, 5.55, atau 0.00024.
+Tipe data Boolean: Tipe data yang berisi nilai true atau false.
+Tipe data Char: Tipe data untuk 1 karakter, seperti ‘a’, ‘Z’ atau ‘%’.
+Non-primitive data type, di antaranya:
+
+Tipe data String: Tipe data untuk kumpulan karakter, seperti “Andi”, “Duniailkom”, atau “Belajar Java”.
+Tipe data Array: Tipe data untuk kumpulan tipe data lain yang sejenis.
+Tipe data Object: Tipe data khusus yang bisa menampung beragam data, termasuk memiliki function/method sendiri.
+*/
+package Basic.variable;
+
+public class Variablle {
+    public static void main(String[] args) {
+        System.out.println(
+                "");
+
+    }
+
+
+}
